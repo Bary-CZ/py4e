517f41aeda7ba4be0f181da44f30316eeead8062
Test <bary12202@gmail.com>
2022-08-29 21:58:49 +0200

    ověření věku!

---
+++ teorie.py
@@ -0,0 +1,28 @@
+#note's for bary!
+x = "hello " + "there" #strings + concatenate = put together
+print(x)
+ddd = 5 - 1  # interger
+print(ddd)
+
+# x = 1 + hi NELZE
+
+print(type(x)) #string
+print(type("x"))  #type odpoví nám co je to string,variable nebo interger takže "class"!
+print(type(1)) #interger = celá čísla
+
+print(float(100) + 20)    #výsledek 120,0 float = číslo "s desetinnou čárkou"
+
+# u dělení je pokaždé výsledek = (float)
+print(10 / 10) # = 1.0
+
+f = "123"
+f = int(f) #změní ze slova na celé číslo NELZE použít na písmena!!
+g = 1
+print(f + g)
+             #USER INPUT
+name = input("kdo jsi?")
+print ("vítej", name) #počká než uživatel něco napíše!
+
+inp = input("Zadej číslo vytahu v eu a dostaneš podlaží usa")
+usf = int(inp) + 1
+print('usa podlaží', usf )
